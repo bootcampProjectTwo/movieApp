6147cd302755006b1fc7330f6231efb5da141736
J Brodie Day <jbrodieday@gmail.com>
2021-09-22 21:30:12 -0400

    Restructuring

--- script.js
+++ script.js
@@ -19,15 +19,52 @@
 
 // ==================== START OF JS ===============================
 // 1. 
-const movieApp = {};
+const movieApp = {}
 
 // 3.
-movieApp.apiURL = 'https://api.themoviedb.org/3/discover/movie';
+movieApp.discoverUrl = 'https://api.themoviedb.org/3/discover/movie';
+movieApp.listUrl = 'https://api.themoviedb.org/3/genre/movie/list';
 movieApp.apiKey = 'e70332005f91b878abd0a2a43b066814';
+
+
+// =========== Getting list of genres from endpoint =================================
+// store url & query info
+movieApp.populateOptions = () => {
+    const url = new URL(movieApp.listUrl);
+    url.search = new URLSearchParams({
+        api_key: movieApp.apiKey
+    });
+    // console.log(url.search)
+
+// make the api call & return the json object
+fetch(url)
+    .then((response) => {
+        return response.json();
+    })
+    .then((jsonResponse) => {
+        console.log(jsonResponse)
+    // call the print function while sending the json response to it
+            // the array of genres is one level inside the json object, so need to say 'jsonResponse.genres'
+    movieApp.printDropdowns(jsonResponse.genres)
+    });
+};
+
+// function to print genres to dropdown menus
+movieApp.printDropdowns = (genreData) => {
+    const genreDropdown = document.querySelector('#genre'); /* select dropdown menu */
+    console.log(genreDropdown)
+    console.log(genreData)
+    genreData.forEach((item) => {
+        const dropdownItem = document.createElement('option'); /* create the new elements */
+        dropdownItem.value = item.id; /* Populate each option with value code for making queries later */
+        dropdownItem.innerText = item.name; /* Populate each option with name of genre for user to see */
+        genreDropdown.appendChild(dropdownItem); /* place new elements inside dropdown menu */
+    });
+};
 
 // 4.
 movieApp.getMovies = function(userGenreSelection, userYearSelection) {
-    const url = new URL(movieApp.apiURL);
+    const url = new URL(movieApp.discoverUrl);
 
     url.search = new URLSearchParams({
         api_key: movieApp.apiKey,
@@ -79,70 +116,19 @@ movieApp.displayMovie = function(movies) {
 };
 
 // 5. Call this function in the genre api call function because there are no elements to attach this to until genre options are dynamically created
-movieApp.getGenreId = function() {
-    document.querySelector('#genre').addEventListener('click', function() {
-        movieApp.getMovies(this.value);
+// movieApp.getGenreId = function() {
+//     document.querySelector('#genre').addEventListener('click', function() {
+//         movieApp.getMovies(this.value);
         
-    });
-};
+//     });
+// };
 
-// 2.
-movieApp.init = function() {
-    // movieApp.populateOptions();
-
-    // movieApp.getCast();
-}
-
-// 2a.
-movieApp.init();
-
-// namespace object
-const movieApp = {}
-
-// properties storing url and key
-movieApp.apiUrl = 'https://api.themoviedb.org/3/genre/movie/list';
-movieApp.apiKey = 'c17d6ea449ecfb7983863b0e3f6a5d7d';
-
-// =========== Getting list of genres from endpoint =================================
-// store url & query info
-movieApp.populateOptions = () => {
-    const url = new URL(movieApp.apiUrl);
-    url.search = new URLSearchParams({
-        api_key: movieApp.apiKey
-    });
-    console.log(url.search)
-
-// make the api call & return the json object
-fetch(url)
-    .then((response) => {
-        return response.json();
-    })
-    .then((jsonResponse) => {
-        console.log(jsonResponse)
-    // call the print function while sending the json response to it
-            // the array of genres is one level inside the json object, so need to say 'jsonResponse.genres'
-    movieApp.printDropdowns(jsonResponse.genres)
-    });
-};
-
-// function to print genres to dropdown menus
-movieApp.printDropdowns = (genreData) => {
-    const genreDropdown = document.querySelector('#genre'); /* select dropdown menu */
-    console.log(genreDropdown)
-    console.log(genreData)
-    genreData.forEach((item) => {
-        const dropdownItem = document.createElement('option'); /* create the new elements */
-        dropdownItem.value = item.id; /* Populate each option with value code for making queries later */
-        dropdownItem.innerText = item.name; /* Populate each option with name of genre for user to see */
-        genreDropdown.appendChild(dropdownItem); /* place new elements inside dropdown menu */
-    });
-};
 
 // initializer
 movieApp.init = function() {
     // run function to populate genre dropdown
     movieApp.populateOptions();
-}
+    // movieApp.getCast();
+};
 
 movieApp.init();
-
